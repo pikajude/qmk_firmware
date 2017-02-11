4a2571d1ab284e062a2171684813320541c14d65
Jude Taylor <me@jude.bio>
2017-02-10 20:25:07 -0800

    reimplement default b.face layer0+1

--- keyboards/ps2avrGB/keymaps/default/keymap.c
+++ keyboards/ps2avrGB/keymaps/default/keymap.c
@@ -19,14 +19,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP( \
-        ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, PSCR,HOME,END, \
-        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,     DEL, \
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,     INS, \
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,           PGUP,\
-        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,          UP,  PGDN,\
-        LCTL,LALT,LGUI,          SPC,                     RGUI,RALT,RCTL,LEFT,DOWN,RGHT \
+        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, GRV, BSPC, \
+        TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,   BSLS, \
+        LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,          ENT,  \
+        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,               RSFT, \
+        LCTL,LALT,LGUI,          SPC,                     RGUI,RALT,FN0,RCTL \
+    ),
+    KEYMAP( \
+        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, GRV, BSPC, \
+        TAB,   Q,   UP,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,  BSLS, \
+        LCTL,LEFT,DOWN,RGHT,   F,   G,   H,   J,   HOME,   PGUP,   SCLN,QUOT, ENT,  \
+        LSFT,     Z,   X,   C,   V,   B,   N,   M,   END,  PGDN, SLSH,        RSFT, \
+        LCTL,LALT,LGUI,          SPC,                     RGUI,   RALT,  FN0, RCTL \
     ),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
+  ACTION_LAYER_MOMENTARY(1),
 };
